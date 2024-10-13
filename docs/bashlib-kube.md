b7b56bfde657130c4105d9b6cb4d67f51c5cf0e7
Nicolas GERARD <gerardnico@gmail.com>
2024-10-13 17:09:07 +0200

    Kube Library general get resources function

--- docs/bashlib-kube.md
+++ docs/bashlib-kube.md
@@ -8,27 +8,73 @@ A library of kubernetes functions
 
 ## Index
 
-* [kube::get_pod_by_app_name](#kubeget_pod_by_app_name)
+* [kube::get_app_label](#kubeget_app_label)
+* [kube::get_resources_by_app_name](#kubeget_resources_by_app_name)
+* [kube::get_resource_by_app_name](#kubeget_resource_by_app_name)
 
-### kube::get_pod_by_app_name
+### kube::get_app_label
 
-Returns the pod name and namespace from an app name
+Return the app label used to locate resources
+It will return the label `app.kubernetes.io/name=<app name>`
+This is the common app label as seen on the [common label page](https://kubernetes.io/docs/concepts/overview/working-with-objects/common-labels/)
 
 #### Example
 
 ```bash
-read -r POD_NAME POD_NAMESPACE <<< "$(kube::get_pod_by_app_name "$APP_NAME")"
+APP_LABEL="$(kube::get_app_label "$APP_NAME")"
 ```
 
 #### Arguments
 
 * **$1** (string): The app name
 
-#### Exit codes
+#### Output on stdout
 
-* **1**: if no pod was not found
+* The app label ie `app.kubernetes.io/name=<app name>`
+
+### kube::get_resources_by_app_name
+
+Function to search for resources across all namespaces by app name
+
+#### Example
+
+```bash
+PODS="$(kube::get_resources_by_app_name "$APP_NAME" pod)"
+```
+
+#### Arguments
+
+* **$1** (string): The app name
+* **$2** (string): The resource type (pod, ...)
 
 #### Output on stdout
 
-* The pod name and namespace
+* The resources name and namespace (one resource by line) or an empty string
+
+### kube::get_resource_by_app_name
+
+Function to search for 1 resource across all namespaces by app name
+
+#### Example
+
+```bash
+read -r POD_NAME POD_NAMESPACE <<< "$(kube::get_resource_by_app_name "$APP_NAME" pod)"
+if [ -z "$POD_NAME" ]; then
+    echo "Error: Pod not found with label $(kube::get_app_label $APP_NAME)"
+    exit 1
+fi
+```
+
+#### Arguments
+
+* **$1** (string): The app name
+* **$2** (string): The resource type (pod, ...)
+
+#### Exit codes
+
+* **1**: - if too many resource was found
+
+#### Output on stdout
+
+* The resource name and namespace separated by a space or an empty string
 
--- lib/bashlib-kube.sh
+++ lib/bashlib-kube.sh
@@ -5,23 +5,64 @@
 #
 #
 
+# @description
+#     Return the app label used to locate resources
+#     It will return the label `app.kubernetes.io/name=<app name>`
+#     This is the common app label as seen on the [common label page](https://kubernetes.io/docs/concepts/overview/working-with-objects/common-labels/)
+#
+# @arg $1 string The app name
+# @example
+#    APP_LABEL="$(kube::get_app_label "$APP_NAME")"
+#
+# @stdout The app label ie `app.kubernetes.io/name=<app name>`
+kube::get_app_label(){
+  APP_NAME=$1
+  echo "app.kubernetes.io/name=$APP_NAME"
+}
 
 # @description
-#     Returns the pod name and namespace from an app name
+#     Function to search for resources across all namespaces by app name
 # @arg $1 string The app name
-# @exitcode 1 if no pod was not found
+# @arg $2 string The resource type (pod, ...)
 # @example
-#    read -r POD_NAME POD_NAMESPACE <<< "$(kube::get_pod_by_app_name "$APP_NAME")"
+#    PODS="$(kube::get_resources_by_app_name "$APP_NAME" pod)"
 #
-# @stdout The pod name and namespace
-kube::get_pod_by_app_name(){
-  APP_LABEL="app.kubernetes.io/name=$APP_NAME"
-  # the name label as seen here: https://kubernetes.io/docs/concepts/overview/working-with-objects/common-labels/
-  # label app.kubernetes.io/name=prometheus
-  POD_ID="$(kubectl get pod -o custom-columns=NAME:.metadata.name,NAMESPACE:.metadata.namespace --no-headers -l "$APP_LABEL" -A)"
-  if [ -z "$POD_ID" ]; then
-      echo::err "Error: Pod not found with label $APP_LABEL"
-      return 1
-  fi
-  echo "$POD_ID"
+# @stdout The resources name and namespace (one resource by line) or an empty string
+kube::get_resources_by_app_name() {
+  local APP_NAME=$1
+  local RESOURCE_TYPE=$1
+
+  APP_LABEL=$(kube::get_app_label $APP_NAME)
+  #
+  # With customs columns, it works also. Example:
+  #     kubectl get pod -o custom-columns=NAME:.metadata.name,NAMESPACE:.metadata.namespace --no-headers -l "$APP_LABEL" -A
+  #
+  kubectl get "$RESOURCE_TYPE" --all-namespaces -l "$APP_LABEL" -o jsonpath='{range .items[*]}{.metadata.name}{" "}{.metadata.namespace}{"\n"}{end}' 2>/dev/null
+
+}
+
+# @description
+#     Function to search for 1 resource across all namespaces by app name
+# @arg $1 string The app name
+# @arg $2 string The resource type (pod, ...)
+# @example
+#    read -r POD_NAME POD_NAMESPACE <<< "$(kube::get_resource_by_app_name "$APP_NAME" pod)"
+#    if [ -z "$POD_NAME" ]; then
+#        echo "Error: Pod not found with label $(kube::get_app_label $APP_NAME)"
+#        exit 1
+#    fi
+#
+# @stdout The resource name and namespace separated by a space or an empty string
+# @exitcode 1 - if too many resource was found
+kube::get_resource_by_app_name(){
+  local APP_NAME=$1
+  local RESOURCE_TYPE=$2
+  RESOURCES="$(kube::get_resources_by_app_name "$APP_NAME" "$RESOURCE_TYPE")"
+  RESOURCE_COUNT=$(echo "$RESOURCES" | sed '/^\s*$/d' | wc -l )
+  if [ "$RESOURCE_COUNT" -gt 1 ]; then
+      echo "Error: Multiple $RESOURCE_TYPE found with the label app.kubernetes.io/name=$APP_NAME:"
+      echo "$RESOURCES"
+      exit 1
+  fi;
+  echo "$RESOURCES"
 }
