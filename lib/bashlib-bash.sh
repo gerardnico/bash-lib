059d3a2b53deb96658523b33a0c9313b64fe83ef
Nicolas GERARD <gerardnico@gmail.com>
2024-11-06 16:12:34 +0100

    Added pinentry curses in Askpass

--- lib/bashlib-bash.sh
+++ lib/bashlib-bash.sh
@@ -69,3 +69,116 @@ bash::function_definition(){
 bash::has_terminal(){
   tty -s
 }
+
+# Pinentry Cli Env Constant
+PINENTRY_CURSES="pinentry-curses"
+PINENTRY_READ="read"
+PINENTRY_ZENITY="zenity"
+PINENTRY_WHIPTAIL="whiptail"
+# @description
+#    Return the program that should ask for the pin/secret/password
+#    Pinentry is a OpenPgp term that designs a program to ask a pin/secret/password
+#    This function supports:
+#    * read (console)
+#    * pinentry-curses (console)
+#    * zenity (gui)
+#    * whiptail (gui)
+# @exitcode 1 if no pinentry program could be found for the context
+# @stdout The pinentry program name
+bash::get_pinentry(){
+
+  # if /dev/tty is available (ie in a textual terminal)
+  if sh -c ": >/dev/tty" >/dev/null 2>/dev/null; then
+    # the `:` points means
+    # Do nothing beyond expanding arguments and performing redirections. The return status is zero
+    # https://www.gnu.org/software/bash/manual/bash.html#index-_003a
+
+    if [ -x "$(command -v "$PINENTRY_CURSES")" ]; then
+      echo "$PINENTRY_CURSES"
+      return
+    fi
+
+    # We use read with dev/tty if we can. Why?
+    # With zenity with Git, you get:
+    #   Error: Received disconnect from UNKNOWN port 65535:11: Bye Bye
+    #   Disconnected from UNKNOWN port 65535
+    #   fatal: Could not read from remote repository.
+    if [ -x "$(command -v "$PINENTRY_READ")" ]; then
+      echo::err "In a text terminal context, we could not found $PINENTRY_CURSES or $PINENTRY_READ"
+      return 1
+    fi
+    echo "$PINENTRY_READ"
+    return
+
+  fi
+
+  # In a Gui, Cli IDE
+  if [ -x "$(command -v "$PINENTRY_ZENITY")" ]; then
+    echo "$PINENTRY_ZENITY"
+    return
+  fi
+  if [ -x "$(command -v "$PINENTRY_WHIPTAIL")" ]; then
+     echo "$PINENTRY_WHIPTAIL"
+     return
+  fi
+
+  echo::err "In a GUI/IDE setting, we could not found the pin client $PINENTRY_ZENITY or $PINENTRY_WHIPTAIL. Please install one of those."
+  return 1
+
+
+}
+
+# @description
+#    Ask for a pin/secret/password for the passed pinentry program
+#    This function would be called in combination with the `get_pinentry` function
+#
+# @example
+#    bash::get_pin "$(bash::get_pinentry)"
+#
+# @args $1 string - The pinentry program
+# @args $2 string - The prompt
+# @exitcode 1 if no pin or a cancel has occurred
+# @exitcode 2 for any other errors (ie no args, unknown pinentry, ...)
+# @stdout The pin
+bash::get_pin(){
+
+  PINENTRY=${1:-}
+  if [ "${PINENTRY}" == "" ]; then
+    echo::err "A pinentry program is mandatory"
+    return 2
+  fi
+  PROMPT=${2:-"Enter the pin/secret/passphrase"}
+
+  case "$1" in
+    "$PINENTRY_ZENITY")
+      if ! PASSWORD=$(zenity --password --title="$PROMPT" 2>/dev/null); then
+        echo::err "User has canceled"
+      else
+        echo "$PASSWORD"
+      fi
+      ;;
+    "$PINENTRY_WHIPTAIL")
+      whiptail --passwordbox "$1:" 8 78 3>&1 1>&2 2>&3
+      ;;
+    "$PINENTRY_READ")
+      echo "$PROMPT" > /dev/tty
+      read -r -s password < /dev/tty
+      echo "$password"
+      ;;
+    "$PINENTRY_CURSES")
+      echo $(pinentry-curses --ttyname "/dev/tty" --lc-ctype "$LANG" <<EOF | grep D | sed 's/^..//'
+SETTIMEOUT 30
+SETPROMPT $PROMPT
+SETOK Ok
+SETCANCEL Cancel
+GETPIN
+BYE
+EOF
+)
+      ;;
+    *)
+      echo:err "The pinentry value ($1) is unknown"
+      exit 2
+      ;;
+  esac
+}
